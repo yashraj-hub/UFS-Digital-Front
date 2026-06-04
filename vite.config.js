f5ee346327ce2bf24af5e8410d1300be92d50d53
unknown <yashrajnayak08@gmail.com>
2026-06-04 16:41:59 +0530

    feat: activity logs, careers,

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,6 @@ export default defineConfig({
       output: {
         manualChunks: {
           "vendor-react": ["react", "react-dom", "react-router-dom"],
-          "vendor-tiptap": [
-            "@tiptap/react",
-            "@tiptap/starter-kit",
-            "@tiptap/pm",
-          ],
         },
       },
     },
